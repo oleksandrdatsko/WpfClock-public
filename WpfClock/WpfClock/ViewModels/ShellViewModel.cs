362b6f85dbf01e4bc1f0225f12944fd0e02033ce
oleksandrdatsko <54150627+oleksandrdatsko@users.noreply.github.com>
2020-05-10 21:34:47 +0200

    Initial commit - MVVM set up

---
+++ WpfClock/WpfClock/ViewModels/ShellViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClock.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+
+    }
+}
